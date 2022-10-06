1ba992aa668373785de36192608cc547822885e4
asbass <93435375+asbass@users.noreply.github.com>
2022-10-06 11:04:04 +0700

    entity, dao

---
+++ src/main/java/com/dao/DAO_form.java
@@ -0,0 +1,10 @@
+package com.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.entity.form;
+
+
+public interface DAO_form  extends JpaRepository<form, Integer>{
+
+}
