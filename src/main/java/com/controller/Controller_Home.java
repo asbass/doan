355f4289538c0b3d7b77a8bb46cc95ebccad0e5f
LuatDo20 <DoLuats20@gmail.com>
2022-10-19 15:37:02 +0700

    update index

--- src/main/java/com/controller/Controller_Home.java
+++ src/main/java/com/controller/Controller_Home.java
@@ -1,13 +1,27 @@
 package com.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.dao.DAO_Product;
+import com.entity.Product;
 
 @Controller
 public class Controller_Home {
+    
+    @Autowired
+    DAO_Product pd;
 	
 	@GetMapping({"","home/index"})
-	public String home() {
+	public String home(Model model) {
+	    List<Product> list = pd.findAll();
+	    if(list != null) {
+	        model.addAttribute("items",list);
+	    }
 		return "index";
 	}
 	
